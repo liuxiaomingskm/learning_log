945985520ed1dc45f1255161cc41eed0b057ad5c
liuxiaomingskm <liuxiaomingskm@gmail.com>
2019-06-13 14:03:09 +0800

    some change.

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from learning_logs.models import Topic,Entry
-# Register your models here.
 
-# 向管理网站注册topic
+from learning_logs.models import Topic, Entry
+
 admin.site.register(Topic)
 admin.site.register(Entry)
--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,20 @@
-"""为应用程序users定义URL模式"""
+"""Defines url patterns for users."""
+
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth import views as auth_views
 
 from . import views
-app_name = 'users'
 
+app_name = 'users'
 urlpatterns = [
-    # 登录页面
-    path('login/', LoginView.as_view(template_name='users/login.html'), name="login"),
-    # 注销
-    path('logout/', views.logout_view, name='logout'),
-     # 注册页面
+	# Login page.
+    path('login/',
+    	auth_views.LoginView.as_view(template_name='users/login.html'),
+    	name='login'),
+
+    # Logout page.
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # Registration page.
     path('register/', views.register, name='register'),
-    ]
+]
